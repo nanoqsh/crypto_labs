39557ed14eb1934ee39ba3d523cabd5fe2186fd4
nanonicholson <nanonicholson@gmail.com>
2019-12-08 21:28:29 +0600

    Functions added

--- lab_8.py
+++ lab_8.py
@@ -3,6 +3,38 @@
 import sys
 
 
+def print_bits(bits, intro=''):
+    print(intro, end='')
+    for bit in bits:
+        print('1' if bit else '0', end='')
+    print()
+
+    return bits
+
+
+def str_to_bits(s, n=64):
+    if len(s) != n:
+        raise Exception('Invalid len of string', s, 'in str_to_bits')
+
+    for c in s:
+        if c != '0' and c != '1':
+            raise Exception('Invalid string', s, 'in str_to_bits')
+
+    return tuple(map(lambda c: c == '1', s))
+
+
+def n_to_bit4(n):
+    b4 = n & 1
+    b3 = (n >> 1) & 1
+    b2 = (n >> 2) & 1
+    b1 = (n >> 3) & 1
+    return (b1, b2, b3, b4)
+
+
+def bits_to_n(bits):
+    return sum([int(bit) * 2 ** i for i, bit in enumerate(reversed(bits))])
+
+
 sn = [
     [],
     [
@@ -56,41 +88,59 @@ sn = [
 ]
 
 
-def n_to_u64(n):
-    return n.to_bytes(8, byteorder='big')
+def bi(bits6, i):
+    b1, b2, b3, b4, b5, b6 = bits6
+    return n_to_bit4(sn[i][bits_to_n((b1, b6))][bits_to_n((b2, b3, b4, b5))])
 
 
-def n_to_bit4(n):
-    b4 = n & 1
-    b3 = (n >> 1) & 1
-    b2 = (n >> 2) & 1
-    b1 = (n >> 3) & 1
-    return (b1, b2, b3, b4)
+def ip(bits64):
+    t = [
+        58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28,	20,	12,	4,
+        62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7,
+    ]
+
+    return tuple(bits64[i - 1] for i in t)
 
 
-def bit4_to_n(bits):
-    b1, b2, b3, b4 = bits
-    return int(b1) * 8 + int(b2) * 4 + int(b3) * 2 + int(b4)
+def ip_inverted(bits64):
+    t = [
+        40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25,
+    ]
+
+    return tuple(bits64[i - 1] for i in t)
 
 
-def list_to_u64(ls):
-    if len(ls) != 8:
-        raise Exception('Len of the list not equals 8')
+def e(bits32):
+    t = [
+        32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9,
+        8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
+        16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
+        24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1,
+    ]
 
-    return bytes(ls)
+    return tuple(bits32[i - 1] for i in t)
 
 
-def print_u64(n):
-    for byte in n:
-        print(hex(byte), end=' ')
-    print()
+def p(bits32):
+    t = [
+        16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
+        2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25,
+    ]
 
-    return n
+    return tuple(bits32[i - 1] for i in t)
+
+
+def xor(a, b):
+    return tuple(map(lambda p: p[0] != p[1], zip(a, b)))
 
 
 def main(argv):
-    for x in range(16):
-        print(x == bit4_to_n(n_to_bit4(x)), x)
+    print_bits(bi((1, 0, 1, 1, 1, 1), 3))
 
 
 main(sys.argv)
