e3910bc17e59d469cde6e821e810f8c8b240f3a1
nanonicholson <nanonicholson@gmail.com>
2019-12-09 15:24:01 +0600

    Lab 8

--- lab_8.py
+++ lab_8.py
@@ -12,6 +12,15 @@ def print_bits(bits, intro=''):
     return bits
 
 
+def print_hex(bits, intro=''):
+    print(intro, end='')
+    for n in range(0, len(bits), 8):
+        print(hex(bits_to_n(bits[n:n+8]))[2:], end=' ')
+    print()
+
+    return bits
+
+
 def str_to_bits(s, n=64):
     if len(s) != n:
         raise Exception('Invalid len of string', s, 'in str_to_bits')
@@ -23,12 +32,16 @@ def str_to_bits(s, n=64):
     return tuple(map(lambda c: c == '1', s))
 
 
-def n_to_bit4(n):
-    b4 = n & 1
-    b3 = (n >> 1) & 1
-    b2 = (n >> 2) & 1
-    b1 = (n >> 3) & 1
-    return (b1, b2, b3, b4)
+def bytearray_to_bits(arr):
+    res = ()
+    for byte in arr:
+        res += n_to_bits(byte)
+
+    return res
+
+
+def n_to_bits(n, l=8):
+    return tuple((n >> i) & 1 for i in reversed(range(l)))
 
 
 def bits_to_n(bits):
@@ -36,7 +49,6 @@ def bits_to_n(bits):
 
 
 sn = [
-    [],
     [
         [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
         [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
@@ -88,9 +100,9 @@ sn = [
 ]
 
 
-def bi(bits6, i):
+def si(bits6, i):
     b1, b2, b3, b4, b5, b6 = bits6
-    return n_to_bit4(sn[i][bits_to_n((b1, b6))][bits_to_n((b2, b3, b4, b5))])
+    return n_to_bits(sn[i][bits_to_n((b1, b6))][bits_to_n((b2, b3, b4, b5))], 4)
 
 
 def ip(bits64):
@@ -139,8 +151,114 @@ def xor(a, b):
     return tuple(map(lambda p: p[0] != p[1], zip(a, b)))
 
 
+def f(r_bits32, key_bits48):
+    bits48 = xor(e(r_bits32), key_bits48)
+    n = 6
+    res = ()
+    for bits4 in tuple(si(bits48[i:i + n], r) for r, i in enumerate(range(0, 48, n))):
+        res += bits4
+    
+    return p(res)
+
+
+def ls(bits):
+    b1, *bn = bits
+    return (*bn, b1)
+
+
+def expand_to_8bit(bits7):
+    return (0, *bits7) if sum(bits7) % 2 == 0 else (1, *bits7)
+
+
+def expand_to_64bit(bits56):
+    n = 7
+    res = ()
+    for t in tuple(expand_to_8bit(bits56[i:i + n]) for i in range(0, 56, n)):
+        res += t
+
+    return res
+
+
+def pc1(bits64):
+    t = [
+        57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+        10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+        63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+        14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4,
+    ]
+
+    return tuple(bits64[i - 1] for i in t)
+
+
+def pc2(bits56):
+    t = [
+        14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4,
+        26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40,
+        51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32,
+    ]
+
+    return tuple(bits56[i - 1] for i in t)
+
+
+def lshift(bits, i):
+    if [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1][i] == 1:
+        return ls(bits)
+    else:
+        return ls(ls(bits))
+
+
+def split_bits(bits, n):
+    return bits[:n], bits[n:]
+
+
+def gen_keys(bits56):
+    c, d = split_bits(pc1(expand_to_64bit(bits56)), 28)
+    keys = []
+
+    for i in range(16):
+        c = lshift(c, i)
+        d = lshift(d, i)
+        keys.append(pc2((*c, *d)))
+
+    return keys
+
+
+def des(block_bits64, key_bits56, encrypt=True):
+    l, r = split_bits(ip(block_bits64), 32)
+    keys = gen_keys(key_bits56)
+    
+    for i in range(16):
+        k = keys[i] if encrypt else keys[15 - i]
+        l, r = r, xor(l, f(r, k))
+    
+    return ip_inverted((*l, *r))
+
+
 def main(argv):
-    print_bits(bi((1, 0, 1, 1, 1, 1), 3))
+    encrypt = True
+    block = ()
+    key = ()
+
+    for arg in argv:
+        if arg.startswith('key='):
+            key = str_to_bits(arg.split('=')[1], 56)
+
+        if arg.startswith('str_key='):
+            key = bytearray_to_bits(arg.split('=')[1].encode('utf-8'))
+
+        if arg.startswith('block='):
+            block = str_to_bits(arg.split('=')[1], 64)
+
+        if arg.startswith('str_block='):
+            block = bytearray_to_bits(arg.split('=')[1].encode('utf-8'))
+
+        if arg == 'd':
+            encrypt = False
+    
+    if key and block:
+        print_hex(key, 'key: ')
+        print_hex(block, 'block: ')
+        print_hex(des(block, key, encrypt), 'encrypted: ' if encrypt else 'decrypted: ')
 
 
 main(sys.argv)
