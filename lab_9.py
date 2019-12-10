c2de8685f3638155b545818d1bbb72e49ce286e4
nanonicholson <nanonicholson@gmail.com>
2019-12-10 21:44:58 +0600

    Selecting e

--- lab_9.py
+++ lab_9.py
@@ -1,6 +1,26 @@
 # RSA
 
 import sys
+import math
+
+
+def phi_pq(p, q):
+    return (p - 1) * (q - 1)
+
+
+def test_gcd(x, y):
+    return math.gcd(x, y) == 1
+
+
+def select_e(p, q, d):
+    phi_n = phi_pq(p, q)
+    print('phi n:', phi_n)
+
+    for x in range(2, phi_n):
+        if test_gcd(phi_n, x):
+            return x
+    
+    raise Exception('e not found!')
 
 
 def pow_mod(a, b, n):
@@ -17,7 +37,8 @@ def pow_mod(a, b, n):
 
 
 def main(argv):
-    pass
+    e = select_e(61, 71, 9577)
+    print('e:', e)
 
 
 main(sys.argv)
